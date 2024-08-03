130f5035ac356c288a1cbfd6bc3baa941d5c515e
minhngoc24 <167333030+minhngoc24@users.noreply.github.com>
2024-08-03 16:10:27 +0700

    Delete src/project6 directory

--- src/project6/RunDefaultGame.java
+++
@@ -1,9 +0,0 @@
-package project6;
-
-public class RunDefaultGame {
-    public static void main(String[] args)
-    {
-        Game theGame = new Game(600, 400);
-        theGame.runGame();
-    }
-}
